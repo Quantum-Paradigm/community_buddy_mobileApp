3cbb68683b56544513aa378e50fbf734ec00becb
Ahmed Raza <ahmedrazasst@gmail.com>
2023-07-25 11:47:07 +0500

    Making SIgn In page UI

--- lib/signIn.dart
+++ lib/signIn.dart
@@ -10,6 +10,33 @@ class MySignIn extends StatefulWidget {
 class _MySignInState extends State<MySignIn> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      child: const SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign In',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Welcome back! Please enter your details.',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
