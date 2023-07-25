7aa60c8cd9f0c779e12f5653fb3cd76f3fd499da
Ahmed Raza <ahmedrazasst@gmail.com>
2023-07-25 11:22:56 +0500

    Created new pages

---
+++ lib/setPassword.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySetPassword extends StatefulWidget {
+  const MySetPassword({super.key});
+
+  @override
+  State<MySetPassword> createState() => _MySetPasswordState();
+}
+
+class _MySetPasswordState extends State<MySetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/signIn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySignIn extends StatefulWidget {
+  const MySignIn({super.key});
+
+  @override
+  State<MySignIn> createState() => _MySignInState();
+}
+
+class _MySignInState extends State<MySignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
