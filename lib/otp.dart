f959c05cea453008f02e76cef010eac5911ec2f2
Ahmed Raza <ahmedrazasst@gmail.com>
2023-07-26 10:49:10 +0500

    added focus Pin changes in OTP screen

--- lib/otp.dart
+++ lib/otp.dart
@@ -14,24 +14,24 @@ class _MyOtpState extends State<MyOtp> {
     final defaultPinTheme = PinTheme(
       width: 56,
       height: 56,
-      textStyle: TextStyle(
+      textStyle: const TextStyle(
           fontSize: 20,
           color: Color.fromRGBO(30, 60, 87, 1),
           fontWeight: FontWeight.w600),
       decoration: BoxDecoration(
-        border: Border.all(color: Color.fromRGBO(234, 239, 243, 1)),
+        border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
         borderRadius: BorderRadius.circular(20),
       ),
     );
 
     final focusedPinTheme = defaultPinTheme.copyDecorationWith(
-      border: Border.all(color: Color.fromRGBO(114, 178, 238, 1)),
+      border: Border.all(color: const Color.fromRGBO(114, 178, 238, 1)),
       borderRadius: BorderRadius.circular(8),
     );
 
     final submittedPinTheme = defaultPinTheme.copyWith(
       decoration: defaultPinTheme.decoration?.copyWith(
-        color: Color.fromRGBO(234, 239, 243, 1),
+        color: const Color.fromRGBO(234, 239, 243, 1),
       ),
     );
 
@@ -73,7 +73,7 @@ class _MyOtpState extends State<MyOtp> {
             ),
             Pinput(
               // defaultPinTheme: defaultPinTheme,
-              // focusedPinTheme: focusedPinTheme,
+              focusedPinTheme: focusedPinTheme,
               // submittedPinTheme: submittedPinTheme,
               length: 6,
               pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
