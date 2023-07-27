cd20079761b60b6da2700a23d87b4fbf14863b6e
Ahmed Raza <ahmedrazasst@gmail.com>
2023-07-27 12:59:10 +0500

    added nav bar in home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
@@ -11,6 +12,24 @@ class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const GNav(
+        gap: 8,
+        tabs: [
+          GButton(
+            icon: Icons.home,
+            text: 'Home',
+          ),
+          GButton(icon: Icons.people, text: 'Residents'),
+          GButton(
+            icon: Icons.construction,
+            text: 'Maintainence',
+          ),
+          GButton(
+            icon: Icons.notifications,
+            text: 'Broadcast',
+          ),
+        ],
+      ),
       body: Container(
         color: Colors.white,
         margin: const EdgeInsets.fromLTRB(25, 0, 25, 0),
