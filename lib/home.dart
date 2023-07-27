dec6aa4d6efca7f901cd197f8cd3641ef6e3308a
Ahmed Raza <ahmedrazasst@gmail.com>
2023-07-27 15:49:59 +0500

    Added doghnut chart in home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:d_chart/d_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
@@ -37,7 +38,56 @@ class _MyHomeState extends State<MyHome> {
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text('data')],
+            children: [
+              AspectRatio(
+                aspectRatio: 14 / 9,
+                child: Stack(
+                  children: [
+                    DChartPie(
+                      data: const [
+                        {'domain': 'Collected', 'measure': 35},
+                        {'domain': 'Current Due', 'measure': 50},
+                        {'domain': 'Over Due', 'measure': 15},
+                      ],
+                      fillColor: (pieData, index) {
+                        switch (pieData['domain']) {
+                          case 'Collected':
+                            return Colors.green;
+                          case 'Current Due':
+                            return Colors.purple;
+                          case 'Over Due':
+                            return Colors.red;
+                        }
+                      },
+                      labelFontSize: 14,
+                      labelPosition: PieLabelPosition.outside,
+                      labelLineColor: Colors.grey,
+                      donutWidth: 20,
+                      showLabelLine: true,
+                      labelColor: Colors.black,
+                      pieLabel: (pieData, index) => pieData['domain'],
+                      // pieLabel: (Map<dynamic, dynamic> pieData, int? index) {
+                      //   return pieData['domain'];
+                      // }
+                    ),
+                    Center(
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 90, 0, 0),
+                        child: Column(
+                          children: [
+                            Center(child: Text('25,000 PKR')),
+                            Center(child: Text('of 100,000 PKR')),
+                            Center(child: Text('collected this month')),
+                          ],
+                        ),
+                      ),
+                    )
+
+                    //  collected this month
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
